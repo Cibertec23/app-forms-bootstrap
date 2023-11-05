aca82a371050055284b92e88dccbfa58ce767d0a
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-04 20:22:15 -0500

    form-calculo-imc función typescript

--- src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
+++ src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class FormCalculoImcComponent {
 
+  objImc = {
+    nombre: null,
+    peso: null, 
+    talla: null,
+    resultado: 0
+  }
+  mostrarResultado = false;
+  listaPacientes: Paciente = [];
+
+  calcularImc(){
+    let tallam = Number(this.objImc.talla) / 100;
+    let peso = Number(this.objImc.peso);
+    this.objImc.resultado = tallam / (peso * peso);
+    this.listaPacientes.push({
+      id: 1, nombre: String(this.objImc.nombre), imc: this.objImc.resultado
+    });
+    this.mostrarResultado =true;
+  }
+
 }
+
+type Paciente = Array<{id: number, nombre: string, imc: number }>
